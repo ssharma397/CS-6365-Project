e458e1f82b4bbc9044acc64d503c3dff8ff28cad
ssharma397 <37806065+ssharma397@users.noreply.github.com>
2018-04-17 08:56:04 -0400

    Add files via upload

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -62,5 +62,21 @@ def fb():
 	posts=jsonify(page)
 	return posts
 
+@app.route('/twitter/place/',methods=['GET','POST'])
+def twitter_place():
+	consumer_key = 'RWJVq4QaNza8zSp3IHofMrfQn'
+	consumer_secret = 'hGbdMGnFrl9rMEmLxV3awb9FquQNluhQ9Uf4YOMEgiECErL4qu'
+	access_token = '978403297307525125-P0JGDGW8kIQGim9jAPLo3jSKpTqFE8z'
+	access_token_secret = 'kStGABKsD5qPuAcspdy4tlm2xRIzhLY3Zkbrovl0VppqB'
+
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = tweepy.API(auth, wait_on_rate_limit=True)
+	trends1=api.trends_place[1]
+	data=trends1[0]
+	trends=data['trends']
+	names=[trend['name'] for trend in trends]
+	return jsonify(names)
+
 if __name__ == '__main__':
 	app.run()
